ab34709151a7073f03c9dde09a18f4789aadec8f
Fred <akamaatei@gmail.com>
2020-10-01 23:14:48 +0300

    add modified files

---
+++ src/app/data.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  gifs = new BehaviorSubject<any>([]);
+
+  constructor(private http: HttpClient) { }
+
+  getTrendingGIFs() {
+    return this.http.get(`http://api.giphy.com/v1/gifs/trending?api_key=wQU6OY4CZmb55xiM7sKrJStUG2iMKbKj&limit=50
+    `)
+      .subscribe((response: any) => {
+        this.gifs.next(response.data);
+      });
+  }
+
+  searchGifs(gifName: string) {
+    return this.http.get(`http://api.giphy.com/v1/gifs/search?q=${gifName}&api_key=wQU6OY4CZmb55xiM7sKrJStUG2iMKbKj&limit=50
+    `)
+      .subscribe((response: any) => {
+        this.gifs.next(response.data);
+      });
+  }
+
+  getGifs() {
+    return this.gifs.asObservable();
+  }
+} 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphykey: 'wQU6OY4CZmb55xiM7sKrJStUG2iMKbKj'
 };
 
 /*
